cec4a8381ebf5ca7f2ab5c88ae294e668189724a
chuanshanjun <chuanshanjun@gmail.com>
2021-02-16 22:04:14 +0800

    add-1 完成-String拼接

---
+++ src/main/java/org/young/chap3/string_/Main.java
@@ -0,0 +1,49 @@
+package org.young.chap3.string_;
+
+/**
+ * @author:ChuanShanJun
+ * @date:2021/2/16
+ * @description:
+ */
+public class Main {
+
+    /**
+     * <h2>使用字符串拼接,编译器会改成stringbuilder</h2>
+     * javac Main.java
+     * javap -l -c -p Main
+     */
+    private static void easyContact() {
+        String username = "chuanshan";
+        String age = "30";
+        String job = "develop";
+
+        String info = username + age + job;
+        System.out.println(info);
+    }
+
+    /**
+     * <h2>在for循环中使用拼接，每次都会新建StringBuilder</h2>
+     */
+    private static void implicitUseStringBuilder(String[] values) {
+        String result = "";
+
+        for (int i = 0; i < values.length; i++) {
+            result += values[i];
+        }
+
+        System.out.println(result);
+    }
+
+    /**
+     * <h2>优化使用append后，则在for循环中只会append</h2>
+     */
+    private static void explicitUseStringBuilder(String[] values) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            result.append(values[i]);
+        }
+
+        System.out.println(result.toString());
+    }
+}
